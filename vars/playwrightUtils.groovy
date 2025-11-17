4a4f38cabdeec6fd67cbdfd3e1bcd44f3ce10828
= <=>
2025-11-17 19:01:55 +0530

    update

---
+++ vars/playwrightUtils.groovy
@@ -0,0 +1,34 @@
+def installDependencies() {
+    echo "Installing dependencies..."
+    retry(2) {
+        bat "npm ci"
+    }
+}
+
+def runTests() {
+    echo "Running Playwright tests..."
+    retry(2) {
+        bat "npx playwright test"
+    }
+}
+
+def publishAllure() {
+    echo "Publishing Allure Report..."
+    allure([
+        includeProperties: false,
+        jdk: '',
+        results: [[path: "allure-results"]]
+    ])
+}
+
+def publishHTML() {
+    echo "Publishing Playwright HTML Report..."
+    publishHTML([
+        allowMissing: true,
+        alwaysLinkToLastBuild: true,
+        keepAll: true,
+        reportDir: "playwright-report",
+        reportFiles: "index.html",
+        reportName: "Playwright HTML Report"
+    ])
+}
